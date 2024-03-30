04601437b2df96359634a030b80bd748c3434c1c
zesettler <gromovgrin@gmail.com>
2024-03-30 20:28:04 +0200

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Page() {
   return (
-    <Button variant="link">
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   );
 }
