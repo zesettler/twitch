4aceafd992f92d27e7462623523b69b7c3252d12
zesettler <gromovgrin@gmail.com>
2024-04-10 21:27:40 +0300

    navbar

---
+++ app/(browse)/_components/navbar/search.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import qs from "query-string";
+import { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export const Search = () => {
+    return (
+        <form
+        onSubmit={() => {}}
+            className="realative w-full lg:w-[400px] flex-items-center"
+        >
+            <Input 
+                placeholder="Search"
+            />
+        </form>
+    );
+};
